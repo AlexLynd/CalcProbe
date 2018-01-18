822cca2a4ff51f68ca95fddbdbb22b550e664e1c
Alex Lynd <alexlynd@protonmail.com>
2018-01-18 09:59:51 -0800

    Modules for calc_source

--- calc_modules.py
+++ calc_modules.py
@@ -1,1 +1,14 @@
-def calc()
+def calc(key):
+    if key!='on':
+        print('calc')
+        print(key)
+    default(calc)
+    
+def ref(key):
+    print('ref')
+    print(programlist)
+	default(ref)
+    
+def rand(key):
+    print('rand')
+	default(rand)
