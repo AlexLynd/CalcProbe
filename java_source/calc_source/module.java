57e7e8915b4547093654bef89251966c1f50fa97
Alex Lynd <alexlynd@protonmail.com>
2018-02-15 17:32:25 -0800

    some java

---
+++ java_source/calc_source/module.java
@@ -0,0 +1,17 @@
+import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.event.*;
+
+public class module
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    public module()
+    {
+        
+    }
+
+    public void main(){
+        
+    }
+}
