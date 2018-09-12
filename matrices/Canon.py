58d18f0527fc4a1497ef645254cf445adccca8d8
Alex Lynd <alexlynd@protonmail.com>
2018-09-12 14:27:09 -0700

    Format fix

--- matrices/Canon.py
+++ matrices/Canon.py
@@ -1,6 +1,5 @@
-LS82Z= [
-    ['%',7,'=',0,'RM',None],
-    ['M-',8,'M+',1,4,None], 
-    ['clr',9,'/',2,5,None],
-    ['+','-','X',3,6,'on'],
-   [None,None,None,'sqrt','.',None] ]
+LS82Z= [['%',7,'=',0,'RM',None],
+        ['M-',8,'M+',1,4,None], 
+        ['clr',9,'/',2,5,None],
+        ['+','-','X',3,6,'on'],
+        [None,None,None,'sqrt','.',None] ]
