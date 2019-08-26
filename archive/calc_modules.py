99360b0ec9ebe8cb7a26905f5f7f5e3e55249d69
AlexLynd <alexlynd@protonmail.com>
2019-08-26 09:39:27 -0700

    move RPi files to archive

---
+++ archive/calc_modules.py
@@ -0,0 +1,20 @@
+import random
+import math
+import time
+
+def calc(key):						#----Normal Calculator----#
+    if key!='on':
+        print('calc')
+        print(key)
+    default(calc)
+    
+def ref(key):						#----Program Reference----#
+    print('ref')
+    print(programlist)
+	default(ref)
+    
+def rand(key):						#----Number Generator----#
+    print('rand')
+	default(rand)
+
+ 
